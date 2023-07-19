aff3051cc22b5282b386a2cde84f165113abd8ee
Kılıç Ali Temiz <katemiz@gmail.com>
2023-07-19 20:50:41 +0300

    daily commit,nothing special for today

--- resources/views/calculate-area-inertia.blade.php
+++ resources/views/calculate-area-inertia.blade.php
@@ -15,6 +15,24 @@
 
                 <x-title :params="config('toolbox.areainertia')" />
 
+                <div class="tabs">
+                    <ul>
+                      <li class="is-active"><a>Circular</a></li>
+                      <li><a>Rectangular</a></li>
+                    </ul>
+                </div>
+
+
+                <div class="column card" id="circular">
+
+                    Circular
+                </div>
+
+                <div class="column card" id="rectangular">
+
+                    Rectangular
+                </div>
+
             </div>
 
         </div>
